726fa32ba280b9f33f697e303abc4e2139ee1c91
BlueKoopa0127 <bluekoopa@me.com>
2023-11-08 17:29:34 +0900

    表示のUIやった 将来的にはきっとリレーションを選択したら図もハイライトされるようになるんだろうなぁ、みつを

--- components/outputMenu.js
+++ components/outputMenu.js
@@ -1,8 +1,11 @@
 import { useRecoilValue } from 'recoil';
 import { selectedObjectState } from './drawChart';
+import { Grid, Typography, Card, CardContent, Box } from '@mui/material';
+import { useState } from 'react';
 
 export function OutputMenu() {
   const selectedObject = useRecoilValue(selectedObjectState);
+  const [selectedRelation, setSelectedRelation] = useState(null);
 
   if (selectedObject == null) {
     return <div>ノードやエッジをクリックしてください</div>;
@@ -14,10 +17,39 @@ export function OutputMenu() {
       {selectedObject.literature.map((e, i) => {
         return (
           <div key={i}>
-            {e[0]} {e[1]} {e[2]}
+            <Card
+              key={i}
+              onClick={() => {
+                setSelectedRelation(e);
+              }}
+              style={
+                selectedRelation == e
+                  ? { cursor: 'pointer', border: '1px solid red' }
+                  : { cursor: 'pointer' }
+              }
+            >
+              <CardContent>
+                <Grid container spacing={2}>
+                  <Grid item xs={5.5}>
+                    <Typography align="center">{e[0]}</Typography>
+                  </Grid>
+                  <Grid item xs={1}>
+                    →
+                  </Grid>
+                  <Grid item xs={5.5}>
+                    <Typography align="center">{e[1]}</Typography>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+            <Box m={2} />
           </div>
         );
       })}
+      <div>
+        説明文：
+        {selectedRelation == null ? '選択してくれ〜' : selectedRelation[2]}
+      </div>
     </div>
   );
 }
